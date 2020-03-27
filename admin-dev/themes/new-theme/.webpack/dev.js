9799bcf63ce3df3185d88180f454b96344096683
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-03-27 10:27:38 +0100

    Add browsersync to webpack

--- admin-dev/themes/new-theme/.webpack/dev.js
+++ admin-dev/themes/new-theme/.webpack/dev.js
@@ -1,21 +1,29 @@
 const path = require('path');
 const common = require('./common.js');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 /**
  * Returns the development webpack config,
  * by merging development specific configuration with the common one.
  */
 function devConfig() {
-  const dev = Object.assign(
-    common,
-    {
-      devtool: 'inline-source-map',
-      devServer: {
-        hot: true,
-        contentBase: path.resolve(__dirname, '/../public'),
-        publicPath: '/',
-      },
-    },
+  const dev = Object.assign(common, {
+    devtool: 'inline-source-map',
+    devServer: {
+      hot: true,
+      contentBase: path.resolve(__dirname, '/../public'),
+      publicPath: '/'
+    }
+  });
+
+  dev.plugins.push(new webpack.HotModuleReplacementPlugin({}));
+  dev.plugins.push(
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3333,
+      proxy: 'http://localhost/'
+    })
   );
 
   return dev;
